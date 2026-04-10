48c9d0a37d82c419462ef6bdf35d632622b91cef
Saulo <saulosantos229@gmail.com>
2026-04-09 23:03:01 -0300

    Refactor code formatting and update application properties for database configuration

--- services/api-gateway-auth/src/test/java/com/apigatewayauth/ApiGatewayAuthApplicationTests.java
+++ services/api-gateway-auth/src/test/java/com/apigatewayauth/ApiGatewayAuthApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApiGatewayAuthApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
